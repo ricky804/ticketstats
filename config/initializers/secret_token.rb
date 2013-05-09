c97aef4652cdb4344c2b4273e12b832377ca61da
ricky <ricky840@gmail.com>
2013-05-08 22:47:46 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketstats::Application.config.secret_token = '787183651c0b509555f2876bae456e180090e7ba40d3b8b6754d06e1d220c0b90dc9d552b494d5bbfed5005fd6a8b78672fe3dcce26f847631c8f9ed8b975ffa'
